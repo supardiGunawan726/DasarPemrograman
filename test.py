34429ce14d73a9c5c3bc9b8fa2dfd4bcb639d328
Supardi G <xblade131@gmail.com>
2023-01-14 22:23:42 +0700

    sync

--- test.py
+++ test.py
@@ -1,14 +1,2 @@
-# Enter your code here. Read input from STDIN. Print output to STDOUT
-from email import utils
-import re
-import os
-
-def isEmailValid(email):
-    return bool(re.match(r'^[a-zA-Z]+[0-9-._]*[a-zA-Z]*@[a-zA-Z]+\.[a-zA-Z]{1,3}$', email))
-
-for _ in range(int(input())):
-    line = input()
-    email_tuple = utils.parseaddr(line)
-
-    if (isEmailValid(email_tuple[1])):
-        print(utils.formataddr(email_tuple))
+for i in range(1,11):pu
+    print(i)
